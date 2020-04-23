d04a69365fdc9092869a86935e6caf3bb80755e9
denghui <a873023670>
2020-04-23 16:26:01 +0800

    按需加载配置完毕

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import { Button } from 'ant-design-vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.component(Button.name, Button);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
